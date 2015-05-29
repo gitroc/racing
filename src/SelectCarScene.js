c14a9a0ec3f5002eadf1a2ba46987fdded8add4d
daiyan <daiyan@chexiang.com>
2015-05-29 12:39:43 +0800

    添加选车事件

--- src/SelectCarScene.js
+++ src/SelectCarScene.js
@@ -1,14 +1,16 @@
 //赛车选择场景
 var CarLayer = cc.Layer.extend({
-    carBrands:["兰博基尼1","兰博基尼2","兰博基尼3","兰博基尼4"],
-    carUrls:["car02.png","car04.png","car05.png","car08.jpg"],
-    carIndex:0,
+    carBrands:[],
+    carUrls:[],
+    carIndex:null,
     carText:null,
     carSprite:null,
     ctor:function(){
         this._super();
         var size = cc.winSize;
-        carIndex = 0;
+        this.carIndex = 0;
+        this.carBrands = ["兰博基尼1","兰博基尼2","兰博基尼3","兰博基尼4"];
+        this.carUrls = ["car02.png","car04.png","car05.png","car08.jpg"];
         //Add Background
         this.bgSprite = new cc.Sprite(res.BackGround_png);
 		this.bgSprite.attr({
@@ -44,8 +46,6 @@ var CarLayer = cc.Layer.extend({
 
         //Car Pictures
         carSprite = new cc.Sprite("res/car02.png");
-//        var newtexture = cc.textureCache.addImage("res/car05.png");
-//        carSprite.setTexture(newtexture);
         carSprite.attr({
             x: size.width / 2,
             y: size.height / 2,
@@ -54,39 +54,46 @@ var CarLayer = cc.Layer.extend({
 
         //Game Instruction
         var title = new ccui.Text("游戏规则", "AmericanTypewriter", 20);
-        title.setPosition(cc.p(size.width/2.0,200));
+        title.setPosition(cc.p(size.width/2.0,100));
         this.addChild(title);
-        var instructText =new ccui.Text("1.在5分钟内行驶够10公里，且未闯红灯、未违反交通违规，2.每人每日可最多邀请5个朋友帮忙接力驾驶，奖励规则同上3.每人每日可最多驾驶3次，以最高成绩计入个人驾驶成绩单，邀请朋友进行驾驶后，将不能再次进行游戏", "AmericanTypewriter", 15);
+        var instructText =new ccui.Text("1.在5分钟内行驶够10公里，且未闯红灯、未违反交通违规，2.每人每日可最多邀请5个朋友帮忙接力驾驶，奖励规则同上3.每人每日可最多驾驶3次，以最高成绩计入个人驾驶成绩单，邀请朋友进行驾驶后，将不能再次进行游戏", "AmericanTypewriter", 12);
         instructText.ignoreContentAdaptWithSize(false);
-        instructText.setContentSize(cc.size(400, 300));
+        instructText.setContentSize(cc.size(size.width, 150));
         instructText.setTextHorizontalAlignment(cc.TEXT_ALIGNMENT_CENTER);
-        instructText.setPosition(cc.p(size.width/2.0,0));
+        instructText.setPosition(cc.p(size.width/2.0,10));
         this.addChild(instructText);
     },
     lTouchEvent:function(sender,type){
         switch (type) {
             case ccui.Widget.TOUCH_ENDED:
-                carIndex--;
-                cc.log(carIndex);
-                selectCar(carIndex);
+            cc.log("left"+this.carIndex);
+                if(this.carIndex>0){
+                    this.carIndex--;
+                }else{
+
+                    this.carIndex = this.carBrands.length - 1;
+                }
+                cc.log("left"+this.carIndex);
+                this.selectCar(this.carIndex);
+            break;
         }
     },
     touchEvent:function(sender,type){
         switch (type) {
-            case ccui.Widget.TOUCH_BEGAN:
-            break;
             case ccui.Widget.TOUCH_ENDED:
-                carIndex++;
-                cc.log(carIndex);
-                selectCar(carIndex);
-            break;
-            default:
+                if(this.carIndex<this.carBrands.length-1){
+                    this.carIndex++;
+                }else{
+                    this.carIndex = 0;
+                }
+                cc.log("right"+this.carIndex);
+                this.selectCar(this.carIndex);
             break;
         }
     },
     selectCar:function(num){
-        var carName = carBrands[num];
-        var carUrl = carUrls[num];
+        var carName = this.carBrands[num];
+        var carUrl = this.carUrls[num];
         var newtexture = cc.textureCache.addImage("res/"+carUrl);
         carText.setString(carName);
         carSprite.setTexture(newtexture);
@@ -95,7 +102,6 @@ var CarLayer = cc.Layer.extend({
         return carIndex;
     }
 });
-
 
 var SelectCarScene = cc.Scene.extend({
 	onEnter:function () {
