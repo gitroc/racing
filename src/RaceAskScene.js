ffe67586379bb2624b7eb391c2d0857ec5f123b2
daiyan <daiyan@chexiang.com>
2015-05-29 14:45:27 +0800

    完善询问界面响应事件

--- src/RaceAskScene.js
+++ src/RaceAskScene.js
@@ -5,11 +5,11 @@ var AskLayer = cc.Layer.extend({
 		var size = cc.winSize;
 		//add Slogan
 		var sloganText = new ccui.Text("选择爱车，狂飙梦想", "AmericanTypewriter", 30);
-		sloganText.setPosition(cc.p(size.width/2.0,size.height/2.0));
+		sloganText.setPosition(cc.p(size.width/2.0,size.height/2.0+150));
 		this.addChild(sloganText);
 		//add prompt1
 		var promptText = new ccui.Text("Are You Ready?", "AmericanTypewriter", 30);
-		promptText.setPosition(cc.p(size.width/2.0,size.height/2.0-100));
+		promptText.setPosition(cc.p(size.width/2.0,size.height/2.0));
         this.addChild(promptText);
 
         //add TextButton
@@ -17,40 +17,34 @@ var AskLayer = cc.Layer.extend({
 		backButton.setTouchEnabled(true);
         backButton.loadTextures("res/backtotopnormal.png", "res/backtotoppressed.png", "");
         backButton.setTitleText("随便说说");
-        backButton.x = size.width / 2.0-50;
-        backButton.y = size.height / 2.0-50;
-//        backButton.addTouchEventListener(this.touchEvent ,this);
+        backButton.x = size.width / 4.0;
+        backButton.y = size.height / 4.0;
+        backButton.addTouchEventListener(this.bTouchEvent ,this);
         this.addChild(backButton);
 
         var nextButton = new ccui.Button();
         nextButton.setTouchEnabled(true);
         nextButton.loadTextures("res/backtotopnormal.png", "res/backtotoppressed.png", "");
         nextButton.setTitleText("为梦狂飙");
-        nextButton.x = size.width / 2.0+50;
-        nextButton.y = size.height / 2.0-50;
+        nextButton.x = size.width /4.0*3;
+        nextButton.y = size.height / 4.0;
         nextButton.addTouchEventListener(this.touchEvent ,this);
         this.addChild(nextButton);
 	},
-	 touchEvent: function (sender, type) {
-            switch (type) {
-                case ccui.Widget.TOUCH_BEGAN:
-//                    this._topDisplayLabel.setString("Touch Down");
-                    break;
-                case ccui.Widget.TOUCH_MOVED:
-//                    this._topDisplayLabel.setString("Touch Move");
-                    break;
-                case ccui.Widget.TOUCH_ENDED:
-                cc.log("Button is clicked!");
-                cc.director.replaceScene( cc.TransitionPageTurn(1, new SelectCarScene(), false) );
-//                    this._topDisplayLabel.setString("Touch Up");
-                    break;
-                case ccui.Widget.TOUCH_CANCELED:
-//                    this._topDisplayLabel.setString("Touch Cancelled");
-                    break;
-                default:
-                    break;
-            }
+	bTouchEvent: function (sender, type) {
+        switch (type) {
+            case ccui.Widget.TOUCH_ENDED:
+            cc.log("Button is clicked!");
+            cc.director.replaceScene( cc.TransitionPageTurn(1, new StartScene(), false) );
         }
+    },
+	touchEvent: function (sender, type) {
+        switch (type) {
+            case ccui.Widget.TOUCH_ENDED:
+            cc.log("Button is clicked!");
+            cc.director.replaceScene( cc.TransitionPageTurn(1, new SelectCarScene(), false) );
+        }
+    }
 });
 var RaceAskScene = cc.Scene.extend({
 	onEnter:function () {
