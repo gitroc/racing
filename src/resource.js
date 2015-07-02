379e1de1a244848622a765065bc00d4191d12c1c
daiyan <daiyan@chexiang.com>
2015-07-02 17:15:20 +0800

    delete unused resource and file

--- src/resource.js
+++ src/resource.js
@@ -1,18 +1,14 @@
 var res = {
     Play_N_png:"res/image/loading_btn_on.jpg",
     Play_S_png:"res/image/loading_btn_off.jpg",
-    BackGround_png : "res/main/main_bg_pic.png",
     Tree_plist : "res/main/main_bg_object_tree.plist",
     Stone_plist : "res/main/main_bg_object_stone.plist",
     Car_plist : "res/main/main_car.plist",
     Car_png : "res/main/main_car_back.png",
     Road_png : "res/main/main_bg_road1.png",
     Barrier_plist : "res/main/main_road_barrier.plist",
-    Fire_png : "res/fire.png",
     Start_N_png : "res/start_N.png",
     Start_S_png : "res/start_S.png",
-    Ground_png : "res/bg.png",
-    Prospect_png : "res/prospect.png",
     Background_plist : "res/main/main_bg_road.plist",
     ReadyGo_plist : "res/image/loading_guide.plist",
     LoadingBg_Png : "res/image/loading_guide_bg.png"
