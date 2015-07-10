ae8654779ddf1e0021b69d1bb8b96937cdd07aa5
rocday <rocday@163.com>
2015-07-10 16:04:52 +0800

    fix a bug.

--- src/sprite/BarrierSprite.js
+++ src/sprite/BarrierSprite.js
@@ -81,8 +81,6 @@ var BarrierSprite = cc.Sprite.extend({
             return;
         } else if (value + 20 == GC.Barrier_Map_Max) {
             value -= 1;
-        } else if (value == 0) {
-            value += 1;
         }
 //        value =0;
         cc.log("LoadingMaps = ", value);
